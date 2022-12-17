dc4aa90e26f750c6407184c1718a0fe8e9b204d8
Xorek123 <115923870+Xorek123@users.noreply.github.com>
2022-12-17 19:36:05 +0300

    final

---
+++ laba3_regex.cpp
@@ -0,0 +1,27 @@
+﻿#include <regex>
+#include <string>
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+
+int main()
+{
+    string filenames = "laba31test.html";
+    regex Emailcheck("^[^_.][a-zA-Z0-9_.-]+[^_.]@[a-zA-Z0-9]+\.[a-zA-Z0-9.]+$");
+    string lineHtml;
+    ifstream fileHtml(filenames);
+    if (!fileHtml.is_open()) {
+        cout << "File error";
+    }
+    else {
+        while (getline(fileHtml, lineHtml))
+        {
+            lineHtml.erase(remove(lineHtml.begin(), lineHtml.end(), ' '), lineHtml.end());
+            if (regex_search(lineHtml, Emailcheck)) {
+                cout << lineHtml << '\n';
+            }
+        }
+    }
+}
+
